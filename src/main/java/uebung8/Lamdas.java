18ad65be7a82eafa350a374e6887e393411058e8
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2024-01-18 15:08:09 +0100

    Update Exceptions

---
+++ src/main/java/uebung8/Lamdas.java
@@ -0,0 +1,10 @@
+package uebung8;
+
+public class Lamdas {
+
+    // Entwerfe eine Liste von Spielkarten
+    // Schreibe ein Consumer für die Ausgabe der Spielkarten
+    // Schreibe ein Predicate um bestimmte Karten (Farbe oder Kartenwert) aus dem Deck zu entfernen
+    // Schreibe einen Supplier, der ein Deck mit mit 20 Zufallskarten liefert
+
+}
